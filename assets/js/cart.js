1bf99232e7136a70d29352c18ee420f9c42bc7a5
siddartha ps <vinayd098@gmail.com>
2024-01-05 13:53:46 +0530

    changed font style

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -10,4 +10,31 @@ $('a.remove').click(function(){
       $('li.items').show(400);
     })
   
+  
+    const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
+const quantityBtn = document.getElementById('quantity');
+const prodTotal = document.getElementById('prodTotal');
+const price = document.getElementById('price');
+
+let count = 0;
+let priceNum = parseFloat(price.innerText);
+let quantityBtnNum = parseFloat(quantityBtn.innerText);
+console.log('Initial priceNum:', priceNum);
+
+incrementBtn.addEventListener('click', () => {
+  count++;
+  quantityBtn.innerText = count;
+});
+
+decrementBtn.addEventListener('click', () => {
+  if (count > 0) {
+    count--;
+    quantityBtn.innerText = count;
+    prodTotal.innerText = (priceNum * count).toFixed(2); 
+  }
+});
+
+    
+
     
